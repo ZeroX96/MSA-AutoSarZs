4cfce2afbb659b97540de97672a6c2b75e3e64cf
Abdulmaguid Eissa <abdulmaguid.eissa@gmail.com>
2019-09-15 12:15:34 +0200

    Adding new file Can_Cfg.h, header file for precompile configurations related to the task

---
+++ CanTeam3/Can_Cfg.h
@@ -0,0 +1,40 @@
+/***************************************************************
+* Module:      Can controller
+* File Name:   Can_Cfg.h
+* Description: Header file for Can controller Module,
+*              implementation is based on AUTOSAR specfications.
+* Author:      Abdulmaguid Eissa, Team(3) Sokara
+* Date:        Sept 14, 2019
+****************************************************************/
+
+#ifndef CAN_CFG_H
+#define CAN_CFG_H
+
+/*
+* SW Module Version is 1.0.0
+*/
+#define CAN_CFG_SW_MAJOR_VERSION          (1U)
+#define CAN_CFG_SW_MINOR_VERSION          (0U)
+#define CAN_CFG_SW_PATCH_VERSION          (0U)
+
+/*
+* AUTOSAR Version is 4.3.1 compatible
+*/
+#define CAN_CFG_AR_RELEASE_MAJOR_VERSION  (4U)
+#define CAN_CFG_AR_RELEASE_MINOR_VERSION  (3U)
+#define CAN_CFG_AR_RELEASE_PATCH_VERSION  (1U)
+
+/* Precompile option for Development Error Detect */
+#define CAN_DEV_ERROR_DETECT              (STD_ON)
+
+/* Precompile user configuration */
+#define CAN_MAIN_FUNCTION_WRITE_PERIOD    (PERIOD10)
+
+/* Can transmission processing type */
+#define CAN_TX_PROCESSING                 (POLLING)
+
+/* Can controller Id, if multiple controllers are supported by 
+   the used hardware */
+#define CAN_CONTROLLER_ID                 (CAN_CONTROLLER0)
+
+#endif /* CAN_CFG_H */
